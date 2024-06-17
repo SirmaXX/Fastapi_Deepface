674c59e78daf8a4ee76f64d91dbd46ad3fbe4ee3
sako-ranj <sakoranj2001@gmail.com>
2024-06-17 16:35:18 +0300

    add cors and improve websocket connection

--- Routes/faces.py
+++ Routes/faces.py
@@ -8,20 +8,28 @@ import base64
 facesroute = APIRouter(responses={404: {"description": "Not found"}})
 
 # Load face cascade classifier
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 
 # Start capturing video
 cap = cv2.VideoCapture(0)
 
+# check cap
+if not cap.isOpened():
+    raise HTTPException(status_code=500, detail="Cannot open camera")
+
 class Frame(BaseModel):
     frame: bytes
+
 
 # Global flag to check WebSocket status
 ws_connected = False
 
 # State to hold the last detection results
 last_faces = []
-last_emotion = 'none'
+last_emotion = "none"
+
 
 async def detect_emotion(websocket: WebSocket):
     global last_faces, last_emotion
@@ -43,27 +51,47 @@ async def detect_emotion(websocket: WebSocket):
             frame_count += 1
             if frame_count % recognition_interval == 0:
                 gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+                faces = face_cascade.detectMultiScale(
+                    gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)
+                )
                 if len(faces) > 0:
                     last_faces = faces
                 else:
                     last_faces = []
 
                 if len(last_faces) > 0:
-                    for (x, y, w, h) in last_faces:
-                        face_roi = frame[y:y + h, x:x + w]
-                        result2 = DeepFace.find(face_roi, db_path='references', enforce_detection=False, silent=True)
-                        last_emotion = result2[0]['identity'][0] if len(result2) > 0 and 'identity' in result2[0] and len(result2[0]['identity']) > 0 else 'none'
-
-            if len(last_faces) > 0:
-                for (x, y, w, h) in last_faces:
+                    for x, y, w, h in last_faces:
+                        face_roi = frame[y : y + h, x : x + w]
+                        result2 = DeepFace.find(
+                            face_roi,
+                            db_path="references",
+                            enforce_detection=False,
+                            silent=True, 
+                        )
+                        last_emotion = (
+                            result2[0]["identity"][0].split(".")[0].split("\\")[1]
+                            if len(result2) > 0
+                            and "identity" in result2[0]
+                            and len(result2[0]["identity"]) > 0
+                            else "none"
+                        )
+            if len(last_faces) > 0: 
+                for x, y, w, h in last_faces:
                     # Draw bounding box and emotion on the frame
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    cv2.putText(frame, last_emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+                    cv2.putText(
+                        frame,
+                        last_emotion,
+                        (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.9,
+                        (255, 0, 0),
+                        2,
+                    )
 
-            _, buffer = cv2.imencode('.jpg', frame)
+            _, buffer = cv2.imencode(".jpg", frame)
             frame_base64 = base64.b64encode(buffer).decode()
-            await websocket.send_json({'frame': frame_base64, 'emotion': last_emotion})
+            await websocket.send_json({"frame": frame_base64, "emotion": last_emotion})
 
             await asyncio.sleep(0.033)  # Approx 30 FPS
 
@@ -75,9 +103,11 @@ async def detect_emotion(websocket: WebSocket):
     except WebSocketDisconnect:
         cap.release()
 
+
 @facesroute.get("/")
 async def index():
     return {"message": "Welcome to Real-time Emotion Detection"}
+
 
 @facesroute.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
